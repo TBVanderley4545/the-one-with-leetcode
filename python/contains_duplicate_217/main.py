e05eeab7964e6e69d262ba45fedfe4922ce7b302
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2023-10-22 22:57:26 -0400

    * Contains Duplicate in Python with test cases

---
+++ python/contains_duplicate_217/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+
+        return False
