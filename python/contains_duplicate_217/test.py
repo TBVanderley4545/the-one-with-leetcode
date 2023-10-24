b8042f47fd09da55de9af144e16d6441395bf4b3
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2023-10-23 23:50:51 -0400

    * Setup pylsp and pylint

--- python/contains_duplicate_217/test.py
+++ python/contains_duplicate_217/test.py
@@ -2,9 +2,9 @@ from main import Solution
 
 
 def test():
-    assert Solution().containsDuplicate([1, 2, 3, 4, 5]) == False, "Should be False"
-    assert Solution().containsDuplicate([1, 2, 3, 1, 5]) == True, "Should be True"
-    assert Solution().containsDuplicate([1, 2, 3, 1, 2]) == True, "Should be True"
+    assert Solution().containsDuplicate([1, 2, 3, 4, 5]) is False, "Should be False"
+    assert Solution().containsDuplicate([1, 2, 3, 1, 5]) is True, "Should be True"
+    assert Solution().containsDuplicate([1, 2, 3, 1, 2]) is True, "Should be True"
 
 
 if __name__ == "__main__":
