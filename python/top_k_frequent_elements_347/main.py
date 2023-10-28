552efec7e74b618af970b5e823a41e62160708ec
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2023-10-28 12:54:11 -0400

    * Added 2 solutions for top k frequent elements.

---
+++ python/top_k_frequent_elements_347/main.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        occurenceMap = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for val in nums:
+            occurenceMap[val] = occurenceMap.get(val, 0) + 1
+
+        for key, val in occurenceMap.items():
+            freq[val].append(key)
+
+        res = []
+
+        for i in range(len(freq) - 1, 0, -1):
+            for value in freq[i]:
+                res.append(value)
+                if len(res) == k:
+                    return res
+
+        return res
+
+    def topKFrequentUsingSorted(self, nums: list[int], k: int) -> list[int]:
+        occurenceMap = {}
+
+        for val in nums:
+            occurenceMap[val] = occurenceMap.get(val, 0) + 1
+
+        return sorted(occurenceMap, key=occurenceMap.get, reverse=True)[:k]
