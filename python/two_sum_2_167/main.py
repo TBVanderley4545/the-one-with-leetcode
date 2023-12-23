596414493dd527fb626a7707b081817bf0efa69f
Tyler Vanderley <TBVanderley4545@users.noreply.github.com>
2023-12-22 19:50:08 -0500

    * Added solution for two sum with sorted list

---
+++ python/two_sum_2_167/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        p1, p2 = 0, len(numbers) - 1
+
+        while p1 < p2:
+            currVal = numbers[p1] + numbers[p2]
+
+            if currVal < target:
+                p1 += 1
+            elif currVal > target:
+                p2 -= 1
+            else:
+                return [p1 + 1, p2 + 1]
